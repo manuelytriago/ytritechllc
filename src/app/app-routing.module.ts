f3ffeee2c7da6d35910189e09fab3d41e8bf2f5e
mytriagoco@gmail.com <mytriagoco@gmail.com>
2022-05-28 17:28:09 -0400

    lazy loading was added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './layout/footer/footer.component';
 import { MainBodyComponent } from './main-body/main-body.component';
-import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainHeaderComponent } from './layout/main-header/main-header.component';
+import { ContactComponent } from './contact/contact.component';
+import { DefaultLayoutComponent } from './layout/default-layout/default-layout.component';
 
 const routes: Routes = [ 
-  { path: '', redirectTo: '/mainHeader', pathMatch: 'full' },
-  { path: 'mainHeader', component: MainHeaderComponent },
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children:[
+      {
+        path:'',loadChildren: () => import('./main-body/main-body.module').then(m => m.MainBodyModule)
+      },
+      {
+        path:'contact',loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) 
+      }
+    ]
+  },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  /*{ path: 'mainHeader', component: MainHeaderComponent },
   { path: 'mainBody', component: MainBodyComponent },
-  { path: 'footer', component: FooterComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'footer', component: FooterComponent },*/
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainHeaderComponent } from './main-header/main-header.component';
-import { MainBodyComponent } from './main-body/main-body.component';
-import { FooterComponent } from './footer/footer.component';
+import { MainHeaderComponent } from './layout/main-header/main-header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 import { MainCompComponent } from './main-comp/main-comp.component';
-import { ContactComponent } from './contact/contact.component';
+import { DefaultLayoutComponent } from './layout/default-layout/default-layout.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     MainHeaderComponent,
     AppComponent,
-    MainBodyComponent,
     FooterComponent,
     MainCompComponent,
-    ContactComponent
+    DefaultLayoutComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule
+  ],
+  exports:[
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
