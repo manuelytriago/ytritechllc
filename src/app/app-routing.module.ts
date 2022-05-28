eeb202b525f67ea6b534080379ba3dd70fc02515
mytriagoco@gmail.com <mytriagoco@gmail.com>
2022-05-28 12:35:25 -0400

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { MainBodyComponent } from './main-body/main-body.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
-import { TestFormComponent } from './test-form/test-form.component';
 
 const routes: Routes = [ 
   { path: '', redirectTo: '/mainHeader', pathMatch: 'full' },
   { path: 'mainHeader', component: MainHeaderComponent },
   { path: 'mainBody', component: MainBodyComponent },
   { path: 'footer', component: FooterComponent },
-  { path: 'testForm', component: TestFormComponent },
 
 ];
 
